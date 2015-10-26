7c0380a097d0ee86c8187d2890ba5e5a61f2503c
Andrew <asolesa@yahoo.ca>
2015-10-25 23:20:47 -0400

    Update Program.cs
    Fixed error of not finding the .dll.

--- sBot/Program.cs
+++ sBot/Program.cs
@@ -25,11 +25,13 @@ namespace sBot
             {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+                //AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+                AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
                 Application.Run(new Form1());
             }
         }
 
+/*
         static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
             using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("sBot.HtmlAgilityPack.dll"))
@@ -38,6 +40,64 @@ namespace sBot
                 stream.Read(data, 0, data.Length);
                 return Assembly.Load(data);
             }
+        }
+*/
+
+        static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            try
+            {
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("sBot.Properties.Resources.HtmlAgilityPack.dll"))
+                {
+                    byte[] data = new byte[stream.Length];
+                    stream.Read(data, 0, data.Length);
+                    return Assembly.Load(data);
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        static Assembly OnResolveAssembly(object sender, ResolveEventArgs e)
+        {
+            var thisAssembly = Assembly.GetExecutingAssembly();
+
+            var assemblyName = new AssemblyName(e.Name);
+            var dllName = assemblyName.Name + ".dll";
+
+            var resources = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(dllName));
+
+            if (resources.Any())
+            {
+                var resourceName = resources.First();
+
+                using (var stream = thisAssembly.GetManifestResourceStream(resourceName))
+                {
+                    if (stream == null)
+                    {
+                        return null;
+                    }
+
+                    var block = new byte[stream.Length];
+
+                    try
+                    {
+                        stream.Read(block, 0, block.Length);
+                        return Assembly.Load(block);
+                    }
+                    catch (IOException)
+                    {
+                        return null;
+                    }
+                    catch (BadImageFormatException)
+                    {
+                        return null;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
